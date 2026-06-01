a12248c643413739ac510c8c693d41f466aab602
Ellsworth McCullough <81348353+ellsworthmmcc@users.noreply.github.com>
2026-06-01 11:56:22 -0700

    Create 6-1-2026-Python.py

---
+++ NeetCode/MergeTwoSortedLinkedLists/6-1-2026-Python.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+# Iterative
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        # Time Complexity:  O(n)
+        # Space Comexplity: O(1)
+
+        dummy = tail = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        
+        tail.next = list1 if list1 else list2
+        return dummy.next
+
+
+# Recursive
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+        
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
