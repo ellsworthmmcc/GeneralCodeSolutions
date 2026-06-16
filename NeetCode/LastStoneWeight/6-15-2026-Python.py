f4a6f8b10dd96c4022db967a8833f1be8f40b32b
Ellsworth McCullough <81348353+ellsworthmmcc@users.noreply.github.com>
2026-06-15 21:52:52 -0700

    Create 6-15-2026-Python.py

---
+++ NeetCode/LastStoneWeight/6-15-2026-Python.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # Time Complexity:  O(n + w)
+        # Space Complexity: O(w)
+        # n is the number of stones, w is the maximum stone weight
+
+        maxStone = max(stones)
+        bucket = [0] * (maxStone + 1)
+        for stone in stones:
+            bucket[stone] += 1
+
+        first = second = maxStone
+        while first > 0:
+            if bucket[first] % 2 == 0:
+                first -= 1
+                continue
+            
+            j = min(first - 1, second)
+            while j > 0 and bucket[j] == 0:
+                j -= 1
+
+            if j == 0:
+                return first
+            second = j
+            bucket[first] -= 1
+            bucket[second] -= 1
+            bucket[first - second] += 1
+            first = max(first - second, second)
+        return first
