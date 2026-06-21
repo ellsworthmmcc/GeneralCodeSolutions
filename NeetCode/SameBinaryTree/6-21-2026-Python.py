ccee4cc5e98bbccb60f51005b72768d0a18e443f
Ellsworth McCullough <81348353+ellsworthmmcc@users.noreply.github.com>
2026-06-21 13:21:55 -0700

    Create 6-21-2026-Python.py

---
+++ NeetCode/SameBinaryTree/6-21-2026-Python.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    # Recursive
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Time Complexity:  O(n)
+        # Space Complexity: O(n)
+
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+    
+    # Iterative
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Time Complexity:  O(n)
+        # Space Complexity: O(n)
+
+        stackP = [p]
+        stackQ = [q]
+
+        while stackP or stackQ:
+            if len(stackP) != len(stackQ):
+                return False
+            
+            nodeP = stackP.pop()
+            nodeQ = stackQ.pop()
+
+            if nodeP is None and nodeQ is None:
+                continue
+            if nodeP is None or nodeQ is None:
+                return False
+            if nodeP.val != nodeQ.val:
+                return False
+            
+            stackP.append(nodeP.left)
+            stackP.append(nodeP.right)
+
+            stackQ.append(nodeQ.left)
+            stackQ.append(nodeQ.right)
+
+        return True
